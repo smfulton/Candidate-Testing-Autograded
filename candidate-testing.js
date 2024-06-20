259d1f23cf4e4449c60f799e724b0ad7e3400c1d
smfulton <sfulton1996@gmail.com>
2024-06-20 17:50:36 -0500

    Updated to only have Part 1 complete.

--- candidate-testing.js
+++ candidate-testing.js
@@ -11,9 +11,9 @@ let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American woman in space? ","True or false: 5 kilometer == 5000 meters? ","(5 + 3)/2 * 10 = ? ","Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ","What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride","true","40","Trajectory","3"];
-let candidateAnswers = [];
+let questions;
+let correctAnswers;
+let candidateAnswers;
 
 
 function askForName() {
@@ -23,46 +23,23 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-  questions.forEach((question,index)=>{
-    candidateAnswers[index] = input.question(question);
-  });
+  candidateAnswer = input.question(question);
+
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-  console.log(`
-    1) ${questions[0]}
-    Your Answer: ${candidateAnswers[0]}
-    Correct Answer: ${correctAnswers[0]}
-
-    2) ${questions[1]}
-    Your Answer: ${candidateAnswers[1]}
-    Correct Answer: ${correctAnswers[1]}
-    
-    3) ${questions[2]}
-    Your Answer: ${candidateAnswers[2]}
-    Correct Answer: ${correctAnswers[2]}
-    
-    4) ${questions[3]}
-    Your Answer: ${candidateAnswers[3]}
-    Correct Answer: ${correctAnswers[3]}
-    
-    5) ${questions[4]}
-    Your Answer: ${candidateAnswers[4]}
-    Correct Answer: ${correctAnswers[4]}
-  `);
+  if(candidateAnswer.toLowerCase() === correctAnswer.toLowerCase()){
+    console.log("Answer correct.");
+  } else{
+    console.log("Answer incorrect.");
+  }
 
 
-  let grade = 0;  //TODO 3.2 use this variable to calculate the candidates score.
+  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
-  candidateAnswers.forEach((answer,index)=>{
-    if(answer.toLowerCase() === correctAnswers[index].toLowerCase()){
-      grade++;
-    }
-  });
-  grade = (grade/questions.length)*100;
+
   return grade;
 }
 
